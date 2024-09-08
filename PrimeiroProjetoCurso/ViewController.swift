67d694c95175be063a6383c1fd53fe08a212c1ff
Ronan Soares <soaresronansilva@gmail.com>
2024-09-07 21:50:38 -0300

    Initial Commit

---
+++ PrimeiroProjetoCurso/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PrimeiroProjetoCurso
+//
+//  Created by Ronan Soares on 05/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loguinLabel: UILabel!
+    @IBOutlet weak var nameUserLabel: UITextField!
+    @IBOutlet weak var passwordUserLabel: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
